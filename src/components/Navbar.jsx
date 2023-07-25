bd5cf7cacfd6387a425a3d569c76de481bcac0f4
aadil124 <ansariaadil124@gmail.com>
2023-07-25 12:43:24 +0530

    added assets

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-light bg-light static-top">
+      <div class="container">
+        <a class="navbar-brand" href="#!">
+          Start Bootstrap
+        </a>
+        <a class="btn btn-primary" href="#signup">
+          Sign Up
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
