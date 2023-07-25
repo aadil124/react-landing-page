0d46691e8195d2c9a803ecd88bc531be05821c07
aadil124 <ansariaadil124@gmail.com>
2023-07-26 00:36:08 +0530

    added navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-light bg-light static-top">
-      <div class="container">
-        <a class="navbar-brand" href="#!">
-          Start Bootstrap
+    <nav className="navbar navbar-light bg-light static-top p-3">
+      <div className="container">
+        <a className="navbar-brand fw-bold fs-2" href="/">
+          React Landing Page
         </a>
-        <a class="btn btn-primary" href="#signup">
+        <a className="btn btn-primary py-2" href="#signup">
           Sign Up
         </a>
       </div>
